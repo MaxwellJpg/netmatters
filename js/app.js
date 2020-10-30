957eeb8c6364349af0c4411a3de99dfdbbb014ec
MaxwellJpg <MaxwellRJermy@yahoo.co.uk>
2020-10-30 13:25:58 +0000

    add news img animation

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
 Mmenu.configs.offCanvas.page.selector = ".wrapper";
-
-document.addEventListener(
-    "DOMContentLoaded", () => {
-        new Mmenu( "#side-nav", {
-            // options
-            "slidingSubmenus": false,
-            "navbars": [{
-                "use": "false",
-            }],
-            "extensions": [
-               "pagedim-black",
-               "position-right",
-            ]
-        });
-    }
-);
+new Mmenu( "#side-nav", {
+    // options
+    "slidingSubmenus": false,
+    "navbars": [{
+        "use": "false",
+    }],
+    "extensions": [
+       "pagedim-black",
+       "position-right",
+    ]
+});
+// document.addEventListener(
+//     "DOMContentLoaded", () => {
+//         new Mmenu( "#side-nav", {
+//             // options
+//             "slidingSubmenus": false,
+//             "navbars": [{
+//                 "use": "false",
+//             }],
+//             "extensions": [
+//                "pagedim-black",
+//                "position-right",
+//             ]
+//         });
+//     }
+// );
 
 $('.sub').prepend('<i class="fa fa-angle-double-right" aria-hidden="true"></i>');
