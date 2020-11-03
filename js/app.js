3cd57c89943374191e0794c99f361446d021e9e5
MaxwellJpg <maxwellrjermy@yahoo.co.uk>
2020-11-03 15:28:54 +0000

    add foundations of cookie popup

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@ if (window.document.documentMode) {
     $('.hd-wrapper').addClass('ie-hd-style');
     $('.service-list').addClass('ie-service-grid');
     $('.news').addClass('ie-news-grid');
+
+    new Mmenu("#side-nav", {
+        // options
+        "slidingSubmenus": false,
+        "navbars": [{
+            "use": "false",
+        }],
+        "extensions": [
+            "pagedim-black",
+            "position-right",
+        ]
+    });
 }
 
 
@@ -13,35 +25,25 @@ if (window.document.documentMode) {
 */
 Mmenu.configs.offCanvas.page.selector = ".wrapper";
 
-new Mmenu("#side-nav", {
-    // options
-    "slidingSubmenus": false,
-    "navbars": [{
-        "use": "false",
-    }],
-    "extensions": [
-        "pagedim-black",
-        "position-right",
-    ]
-});
-// document.addEventListener(
-//     "DOMContentLoaded", () => {
-//         new Mmenu( "#side-nav", {
-//             // options
-//             "slidingSubmenus": false,
-//             "navbars": [{
-//                 "use": "false",
-//             }],
-//             "extensions": [
-//                "pagedim-black",
-//                "position-right",
-//             ]
-//         });
-//     }
-// );
-//
+document.addEventListener(
+    "DOMContentLoaded", () => {
+        new Mmenu("#side-nav", {
+            // options
+            "slidingSubmenus": false,
+            "navbars": [{
+                "use": "false",
+            }],
+            "extensions": [
+                "pagedim-black",
+                "position-right",
+            ]
+        });
+    }
+);
+
 $('.sub').prepend('<i class="fa fa-angle-double-right" aria-hidden="true"></i>');
 
+// CAROUSEL
 $(function () {
     $('.carousel').square1({
         theme: 'light',
@@ -52,3 +54,23 @@ $(function () {
     });
 });
 
+// COOKIE POPUP 
+function PopUp(hideOrshow) {
+    if (hideOrshow == 'hide') {
+        document.getElementById('ac-wrapper').style.display = "none";
+    }
+    else if (localStorage.getItem("popupWasShown") == null) {
+        localStorage.setItem("popupWasShown", 1);
+        document.getElementById('ac-wrapper').removeAttribute('style');
+    }
+}
+window.onload = function () {
+    setTimeout(function () {
+        PopUp('show');
+    }, 0);
+}
+
+
+function hideNow(e) {
+    if (e.target.id == 'ac-wrapper') document.getElementById('ac-wrapper').style.display = 'none';
+}
