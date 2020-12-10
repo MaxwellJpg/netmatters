d337d2d92f35b10f4a5a5fafe052169b21ed7914
MaxwellJpg <MaxwellRJermy@yahoo.co.uk>
2020-12-10 10:50:19 +0000

    format code for better legibility

--- contact-us.php
+++ contact-us.php
@@ -1,11 +1,13 @@
 <?PHP
-$title = "title";
+$title = "Contact Us";
 include_once($_SERVER['DOCUMENT_ROOT']. '/netmatters/template/header.php');?>
 <!-- main start inc from header -->
 
-<!-- code here -->
-
-
+<div class="section-hd">
+    <div class="wrap">
+        <h1>How can we help you?</h1>
+    </div>
+</div>
 
 
 <!-- main end, inc in footer -->
