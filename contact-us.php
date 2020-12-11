14712a7bc11fcdb29f7b0bd6117712a6131ee6d4
MaxwellJpg <MaxwellRJermy@yahoo.co.uk>
2020-12-11 15:19:11 +0000

    add contact info

--- contact-us.php
+++ contact-us.php
@@ -8,6 +8,26 @@ include_once($_SERVER['DOCUMENT_ROOT']. '/netmatters/template/header.php');?>
         <h1>How can we help you?</h1>
     </div>
 </div>
+<div class="contact-list wrap">
+    <p>
+        <strong>Call us on:</strong><br>
+        <a href="tel:01603704020">01603 70 40 20</a>
+    </p>
+    <p>
+        <strong>Email us on:</strong><br>
+        <a href="mailto:sales@netmatters.com">sales@netmatters.com</a>
+    </p>
+    <p>
+        <strong>Call us at our Gorleston branch on:</strong><br>
+        <a href="tel:01493603204">01493 603204</a>
+    </p>
+    <p>
+        <strong>Business hours:</strong>
+    </p>
+    <p>
+        <strong>Monday - Friday 07:00 - 18:00</strong>
+    </p>
+</div>
 
 
 <!-- main end, inc in footer -->
